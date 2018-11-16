3bb307585ad7fffac5f8a5bbb503f47aa7ef163a
Arhidimon <arhidimon@ukr.net>
2018-11-16 17:28:22 +0200

    working prototype

--- src/client.c
+++ src/client.c
@@ -12,65 +12,42 @@
 
 #include "../include/client.h"
 
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
-#include <string.h>
+#include <sys/socket.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
 
-int main(int argc, char *argv[]) {
-	int sockfd = 0, n = 0;
-	char recvBuff[1024];
+int main(int argc, char const *argv[])
+{
+	struct sockaddr_in address;
+	int sock = 0, valread;
 	struct sockaddr_in serv_addr;
 
-
-	if(argc != 2) {
-		printf("\n Usage: %s <ip of server> \n",argv[0]);
-		return 1;
-	}
-
-	memset(recvBuff, '0',sizeof(recvBuff));
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	char buffer[1024] = {0};
+	// TODO check for argc
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf("\n Error : Could not create socket \n");
-		return 1;
+		printf("\n Socket creation error \n");
+		return -1;
 	}
-
 	memset(&serv_addr, '0', sizeof(serv_addr));
-
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(5000);
-
+	serv_addr.sin_port = htons(PORT);
 	if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
 	{
-		printf("\n inet_pton error occured\n");
-		return 1;
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
 	}
-
-	if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
-		printf("\n Error : Connect Failed \n");
-		return 1;
+		printf("\nConnection Failed \n");
+		return -1;
 	}
-
-	while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-	{
-		recvBuff[n] = 0;
-//		if(fputs(recvBuff, stdout) == EOF)
-//		{
-//			printf("\n Error : Fputs error\n");
-//		}
-	printf("Message[%s]", recvBuff);
-	}
-
-	if(n < 0)
-	{
-		printf("\n Read error \n");
-	}
+	send(sock , argv[2] , strlen(argv[2]) , 0 ); // FIXME ft_strlen
+	printf("String from client sent\n");
+	valread = read( sock , buffer, 1024);
+	printf("Answer:%s\n",buffer );
 	return 0;
 }
